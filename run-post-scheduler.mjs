5b6f34ba03df6741f330d33fee0120d313c3e8c3
ryuhosoy <longpingxigu@gmail.com>
2025-04-08 12:35:28 +0900

    key名一部変更

--- run-post-scheduler.mjs
+++ run-post-scheduler.mjs
@@ -3,8 +3,8 @@ import { TwitterApi } from "twitter-api-v2";
 
 // Supabaseクライアント作成
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
 
 const now = new Date();
