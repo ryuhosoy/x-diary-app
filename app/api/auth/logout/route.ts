362bda4c6a8b85f6759c3a7419350689b1866201
ryuhosoy <longpingxigu@gmail.com>
2025-05-18 10:16:42 +0800

    リダイレクト実装法変更

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,14 +1,14 @@
 // import { NextResponse } from 'next/server';
-// import { cookies } from 'next/headers';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 export async function GET() {
+  // クッキーを削除
+  const cookieStore = await cookies();
+  cookieStore.delete('accessToken');
+  cookieStore.delete('accessSecret');
+  
   // const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/login`);
   redirect(`https://x-diary-app.vercel.app/login`);
-
-  // // クッキーを削除
-  // const cookieStore = await cookies();
-  // cookieStore.delete('accessToken');
-  // cookieStore.delete('accessSecret');
 
   // return response;
 }
