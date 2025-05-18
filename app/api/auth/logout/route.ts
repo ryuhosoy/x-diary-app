924824826aa4b1067b821d0430fde9c68897aff9
ryuhosoy <longpingxigu@gmail.com>
2025-05-18 08:50:53 +0800

    一時的にクッキー削除しないように

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -3,10 +3,10 @@ import { cookies } from 'next/headers';
 export async function POST() {
   const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/login`);
 
-  // クッキーを削除
-  const cookieStore = await cookies();
-  cookieStore.delete('accessToken');
-  cookieStore.delete('accessSecret');
+  // // クッキーを削除
+  // const cookieStore = await cookies();
+  // cookieStore.delete('accessToken');
+  // cookieStore.delete('accessSecret');
 
   return response;
 }
